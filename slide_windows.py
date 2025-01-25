adb91f312f4cc542e2600ce7b3f26df4397e9fdf
Deson Dai <1155211130@link.cuhk.edu.hk>
2025-01-26 00:12:03 +0800

    windows

--- slide_windows.py
+++ slide_windows.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 # 假设你的数据已经被加载到一个 DataFrame 中
 
-df = pd.read_csv('NEW_MEDAL_TABLE_WITH_PARTICIPANTS.csv')
+df = pd.read_csv('Final_table.csv')
 
 # 创建一个以 NOC 为索引的 DataFrame，并获取每个 NOC 对应的参赛年份
 noc_years = df.groupby('NOC')['Year'].apply(list).reset_index()
@@ -27,21 +27,31 @@ for i in range(noc_years.shape[0]):
         continue
 
     windows[noc] = []
-    for j in range(3, len(year)):
+    for j in range(3, len(year)-1):
         tmp_ls = []
         if year[j] == year[j-1]+4 and year[j] == year[j-2]+8 and year[j] == year[j-3]+12:
             for k in range(j-3, j):
+
                 condition = (df['Year'] == year[k]) & (df['NOC'] == noc)
                 find = df[condition]
                 row = find.iloc[0].drop(['Year', 'NOC', 'Rank']).tolist()  # 将行数据转换为列表
 
                 # 将 numpy 数据类型转换为 Python 原生类型
-                row = [item.item() if isinstance(item, np.generic) else item for item in row]
+
                 tmp_ls += row
+
+            k = j+1
+            condition = (df['Year'] == year[k]) & (df['NOC'] == noc)
+            find = df[condition]
+            row = find.iloc[0].drop(['Year', 'NOC', 'Rank', 'Participants']).tolist()
+            row = [item.item() if isinstance(item, np.generic) else item for item in row]
             tmp_ls = clean_list(tmp_ls)
-            windows[noc].append(tmp_ls)
+            row = clean_list(row)
+            tmp = {}
+            tmp['Feats'] = tmp_ls
+            tmp['label'] = row
+            windows[noc].append(tmp)
         else:
             continue
 
-keys = windows.keys()
-print(keys)
+print(windows)
